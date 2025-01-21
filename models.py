d78aebbe1d5f10e1e466acb5eb0962b939a6a294
Manav Goel <manavgoel47@gmail.com>
2025-01-20 20:24:23 -0800

    Adding LSTM

--- models.py
+++ models.py
@@ -52,6 +52,33 @@ class RNN_Block(nn.Module):
         return nn.init.kaiming_uniform_(torch.empty(batch_size, self.hidden_size, device=device))
     
 
+class LSTM_Block(nn.Module):
+
+    def __init__(self, embed_size, hidden_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.forget_layer = layer_init(nn.Linear(embed_size+hidden_size, hidden_size), n_linearity="sigmoid")
+        self.input_layer = layer_init(nn.Linear(embed_size+hidden_size, hidden_size), n_linearity="sigmoid")
+        self.hidden_layer = layer_init(nn.Linear(embed_size+hidden_size, hidden_size), n_linearity="tanh")
+        self.output_layer = layer_init(nn.Linear(embed_size+hidden_size, hidden_size), n_linearity="sigmoid")
+        self.hid2out = layer_init(nn.Linear(hidden_size, hidden_size), n_linearity="tanh")
+
+    def forward(self, x, short_term, long_term):
+        combined = torch.cat((x, short_term), dim=-1)
+        f = F.sigmoid(self.forget_layer(combined))
+        i = F.sigmoid(self.input_layer(combined))
+        C = F.tanh(self.hidden_layer(combined))
+        C_new = torch.mul(f, long_term) + torch.mul(i, C)
+        o = F.sigmoid(self.output_layer(combined))
+        C_out = F.tanh(self.hid2out(C_new))
+        output = torch.mul(C_out, o)
+        return output, C_new
+    
+    def init_hidden(self, batch_size, device="cpu"):
+        return nn.init.kaiming_uniform_(torch.empty(batch_size, self.hidden_size, device=device))
+
+
+
 class SimpleRNN(nn.Module):
     
     def __init__(self, embed_size, hidden_size, vocab_size, device="cpu"):
@@ -70,5 +97,28 @@ class SimpleRNN(nn.Module):
             output, hidden_state = self.block(x[:,t,:], hidden_state)
             out.append(output)
             hid.append(hidden_state)
+        return out, hid
+        
+
+class LSTM(nn.Module):
+    
+    def __init__(self, embed_size, hidden_size, vocab_size, device="cpu"):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.block = LSTM_Block(embed_size, hidden_size)
+        self.fc = layer_init(nn.Linear(hidden_size, vocab_size), n_linearity="linear")
+        self.hidden_size = hidden_size
+        self.device = device
+    
+    def forward(self, x):
+        x = self.embedding(x)
+        batch_size, sequence_length, _ = x.shape
+        long_term = self.block.init_hidden(batch_size, self.device)
+        output = long_term
+        out, hid = [], []
+        for t in range(sequence_length):
+            output, long_term = self.block(x[:,t,:], output, long_term)
+            out.append(self.fc(output))
+            hid.append(long_term)
         return out, hid
         
